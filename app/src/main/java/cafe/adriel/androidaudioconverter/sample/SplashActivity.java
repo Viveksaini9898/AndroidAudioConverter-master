bc2de94e5b9c4140e0237785dbf8a842676d0563
Vivek saini <viveksainip@gmail.com>
2020-10-12 10:47:43 +0530

    change ui

--- app/src/main/java/cafe/adriel/androidaudioconverter/sample/SplashActivity.java
+++ app/src/main/java/cafe/adriel/androidaudioconverter/sample/SplashActivity.java
@@ -13,15 +13,19 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class SplashActivity extends AppCompatActivity {
 
     private Handler handler;
     private static final int REQ_CODE=345;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         handler =new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -30,15 +34,16 @@ public class SplashActivity extends AppCompatActivity {
                     if (ContextCompat.checkSelfPermission(SplashActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                         Intent intent = new Intent(SplashActivity.this, BaseActivity.class);
                         startActivity(intent);
+                        Toast.makeText(SplashActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
                         finish();
                     } else {
                         ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, REQ_CODE);
                     }
-                } else {
+                } /*else {
                     Intent intent = new Intent(SplashActivity.this, BaseActivity.class);
                     startActivity(intent);
                     Toast.makeText(SplashActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
-                }
+                }*/
             }
             },500);
 
